012d1b164e95702ad17f031f3a020d4b872b7c24
Carl Kadie <carlk@msn.com>
2026-01-22 17:34:45 -0800

    better c2

--- demos/c_led/c2_strip_8_color_button_blink.rs
+++ demos/c_led/c2_strip_8_color_button_blink.rs
@@ -1,81 +1,9 @@
-// #![no_std]
-// #![no_main]
-// #![cfg(not(feature = "host"))]
-
-// use core::{convert::Infallible, panic};
-
-// use device_kit::{
-//     Result,
-//     button::{Button, PressDuration, PressedTo},
-//     led_strip::{Frame1d, RGB8, colors, led_strip},
-// };
-// use embassy_executor::Spawner;
-// use embassy_time::Duration;
-// use {defmt_rtt as _, panic_probe as _};
-
-// led_strip! {
-//     LedStrip8 {
-//         pin: PIN_0,
-//         len: 8,
-//         max_frames: 2,
-//     }
-// }
-
-// #[embassy_executor::main]
-// async fn main(spawner: Spawner) -> ! {
-//     let err = inner_main(spawner).await.unwrap_err();
-//     panic!("{err}");
-// }
-
-// async fn inner_main(spawner: Spawner) -> Result<Infallible> {
-//     let p = embassy_rp::init(Default::default());
-
-//     // A button just needs to know its pin and whether it connects to Vcc or Ground.
-//     // (Pico 2 erratum E9 is avoided by wiring buttons to GND.)
-//     let mut button = Button::new(p.PIN_13, PressedTo::Ground);
-
-//     // We also create a LED strip on GPIO0 with length 8.
-//     let led_strip8 = LedStrip8::new(p.PIN_0, p.PIO0, p.DMA_CH0, spawner)?;
-
-//     const BLINK_DELAY: Duration = Duration::from_millis(150);
-//     const COLORS: [RGB8; 4] = [colors::YELLOW, colors::ORANGE, colors::GREEN, colors::BLUE];
-
-//     let mut led_index: usize = 0;
-//     let mut color_index: usize = 0;
-//     let mut base_frame = Frame1d::new();
-
-//     loop {
-//         let color = COLORS[color_index];
-//         let short_frame = base_frame;
-//         let mut long_frame = base_frame;
-//         if led_index < LedStrip8::LEN {
-//             long_frame[led_index] = color;
-//         }
-//         led_strip8.animate([(short_frame, BLINK_DELAY), (long_frame, BLINK_DELAY)])?;
-
-//         match button.wait_for_press_duration().await {
-//             PressDuration::Short => {
-//                 if led_index == LedStrip8::LEN {
-//                     led_index = 0;
-//                     base_frame = Frame1d::new();
-//                 }
-
-//                 base_frame[led_index] = color;
-//                 led_index += 1;
-//             }
-//             PressDuration::Long => {
-//                 color_index = (color_index + 1) % COLORS.len();
-//             }
-//         }
-//     }
-// }
-
 #![no_std]
 #![no_main]
 #![cfg(not(feature = "host"))]
 
+use core::future;
 use core::{convert::Infallible, panic};
-
 use device_kit::{
     Result,
     button::{Button, PressDuration, PressedTo},
@@ -110,27 +38,30 @@ async fn inner_main(spawner: Spawner) -> Result<Infallible> {
     let mut colors = COLORS.iter().cycle();
     let mut color = *colors.next().unwrap();
 
-    loop {
-        let mut short_frame = Frame1d::new();
-        for led_index in 0..LedStrip8::LEN {
-            loop {
-                let mut long_frame = short_frame;
-                long_frame[led_index] = color;
-                led_strip8.animate([(short_frame, BLINK_DELAY), (long_frame, BLINK_DELAY)])?;
+    // Fill with initial color, YELLOW.
+    let mut short_frame = Frame1d::filled(color);
+    let mut long_frame = Frame1d::filled(color);
+    for led_index in (0..LedStrip8::LEN).cycle() {
+        loop {
+            // Make the current LED blink.
+            short_frame[led_index] = colors::BLACK;
+            long_frame[led_index] = color;
+            led_strip8.animate([(short_frame, BLINK_DELAY), (long_frame, BLINK_DELAY)])?;
 
-                // Tells if a long or short press. Returns from a long press before button release.
-                match button.wait_for_press_duration().await {
-                    // On a short press, do as before: set the short frame LED and move on.
-                    PressDuration::Short => {
-                        short_frame[led_index] = color;
-                        break;
-                    }
-                    // On a long press, change color for subsequent LEDs.
-                    PressDuration::Long => {
-                        color = *colors.next().unwrap();
-                    }
+            // Tells if a long or short press. Returns from a long press before button release.
+            match button.wait_for_press_duration().await {
+                // If short, change BLACK to current color and move to next LED.
+                PressDuration::Short => {
+                    short_frame[led_index] = color;
+                    break;
+                }
+                // On a long press, change color for subsequent LEDs.
+                // Loop up to continue work on this pixel.
+                PressDuration::Long => {
+                    color = *colors.next().unwrap();
                 }
             }
         }
     }
+    future::pending().await // run forever
 }
